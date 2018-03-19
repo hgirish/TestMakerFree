2d76a446fa4cd8b8e5b06cc76525cf1440a78722
hgirish <girish@post.com>
2018-03-18 18:36:15 -0700

    result , answer edit validation

--- TestMakerFreeWebApp/ClientApp/src/app/answer-edit/answer-edit.component.ts
+++ TestMakerFreeWebApp/ClientApp/src/app/answer-edit/answer-edit.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit ,Inject} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-
+import { FormControl, FormBuilder, Validators, FormGroup } from '@angular/forms'
 @Component({
   selector: 'answer-edit',
   templateUrl: './answer-edit.component.html',
@@ -11,12 +11,15 @@ export class AnswerEditComponent implements OnInit {
   title: string;
   answer: Answer;
   editMode: boolean;
+  form: FormGroup;
 
   constructor(private activatedRoute: ActivatedRoute,
     private router: Router,
     private http: HttpClient,
+    private fb: FormBuilder,
     @Inject('BASE_URL') private baseUrl: string) {
     this.answer = <Answer>{};
+    this.createForm();
     var id = activatedRoute.snapshot.params["id"];
 
     this.editMode = (this.activatedRoute.snapshot.url[1].path === "edit");
@@ -34,18 +37,35 @@ export class AnswerEditComponent implements OnInit {
       this.title = "Create a new Answer";
     }
   }
+  createForm() {
+    this.form = this.fb.group({
+      Text: ['', Validators.required],
+      Value: ['', [Validators.required, Validators.min(-5), Validators.max(5)]]
+    })
+  }
+  updateForm() {
+    this.form.setValue({
+      Text: this.answer.Text,
+      Value: this.answer.Value
+    })
+  }
+  onSubmit() {
+    var tempAnswer = <Answer>{};
+    tempAnswer.Text = this.form.value.Text;
+    tempAnswer.Value = this.form.value.Value;
+    tempAnswer.QuestionId = this.answer.QuestionId;
 
-  onSubmit(answer: Answer) {
     var url = this.baseUrl + "api/answer";
     if (this.editMode) {
-      this.http.post<Answer>(url, answer).
+      tempAnswer.Id = this.answer.Id;
+      this.http.post<Answer>(url, tempAnswer).
         subscribe(res => {
           var v = res;
           console.log(`Answer ${v.Id} has been updated.`);
           this.router.navigate(['question/edit', v.QuestionId]);
         }, error => console.error(error));
     } else {
-      this.http.put<Answer>(url, answer)
+      this.http.put<Answer>(url, tempAnswer)
         .subscribe(res => {
           var v = res;
           console.log(`Answer ${v.Id} has been created.`);
@@ -59,5 +79,21 @@ export class AnswerEditComponent implements OnInit {
   }
   ngOnInit() {
   }
+  getFormControl(name: string) {
+    return this.form.get(name);
+  }
 
+  isValid(name: string) {
+    var e = this.getFormControl(name);
+    return e && e.valid;
+  }
+  isChanged(name: string) {
+    var e = this.getFormControl(name);
+    return e && (e.dirty || e.touched);
+  }
+
+  hasError(name: string) {
+    var e = this.getFormControl(name);
+    return e && (e.dirty || e.touched) && !e.valid;
+  }
 }
--- TestMakerFreeWebApp/ClientApp/src/app/result-edit/result-edit.component.ts
+++ TestMakerFreeWebApp/ClientApp/src/app/result-edit/result-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { HttpClient } from "@angular/common/http";
+import { FormControl, FormBuilder, Validators, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'result-edit',
@@ -11,12 +12,15 @@ export class ResultEditComponent implements OnInit {
   title: string;
   result: Result;
   editMode: boolean;
+  form: FormGroup;
 
   constructor(private activatedRoute: ActivatedRoute,
     private router: Router,
     private http: HttpClient,
+    private fb: FormBuilder,
     @Inject('BASE_URL') private baseUrl: string) {
     this.result = <Result>{};
+    this.createForm();
 
     var id = +this.activatedRoute.snapshot.params["id"];
 
@@ -28,24 +32,46 @@ export class ResultEditComponent implements OnInit {
         .subscribe(res => {
           this.result = res;
           this.title = "Edit - " + this.result.Text;
+          this.updateForm();
         }, error => console.error(error));
     } else {
       this.result.QuizId = id;
       this.title = 'Create a new Result';
     }
   }
+  createForm() {
+    this.form = this.fb.group({
+      Text: ['', Validators.required],
+      MinValue: ['', Validators.pattern(/^\d*$/)],
+      MaxValue: ['', Validators.pattern(/^\d*$/)]
+    })
+  }
+  updateForm() {
+    this.form.setValue({
+      Text: this.result.Text,
+      MinValue: this.result.MinValue || '',
+      MaxValue: this.result.MaxValue || ''
+    })
+  }
 
-  onSubmit(result: Result) {
+  onSubmit() {
+    var tempResult = <Result>{};
+    tempResult.Text = this.form.value.Text;
+    tempResult.MinValue = this.form.value.MinValue;
+    tempResult.MaxValue = this.form.value.MaxValue;
+    tempResult.QuizId = this.result.QuizId;
+
     var url = this.baseUrl + "api/result";
     if (this.editMode) {
-      this.http.post<Result>(url, result)
+      tempResult.Id = this.result.Id;
+      this.http.post<Result>(url, tempResult)
         .subscribe(res => {
           var v = res;
           console.log(`Result ${v.Id} has been updated.`);
           this.router.navigate(["quiz/edit", v.QuizId]);
         }, error => console.log(error));
     } else {
-      this.http.put<Result>(url, result)
+      this.http.put<Result>(url, tempResult)
         .subscribe(res => {
           var v = res;
           console.log(`Result ${v.Id} has been created.`);
@@ -60,5 +86,21 @@ export class ResultEditComponent implements OnInit {
 
   ngOnInit() {
   }
+  getFormControl(name: string) {
+    return this.form.get(name);
+  }
 
+  isValid(name: string) {
+    var e = this.getFormControl(name);
+    return e && e.valid;
+  }
+  isChanged(name: string) {
+    var e = this.getFormControl(name);
+    return e && (e.dirty || e.touched);
+  }
+
+  hasError(name: string) {
+    var e = this.getFormControl(name);
+    return e && (e.dirty || e.touched) && !e.valid;
+  }
 }
